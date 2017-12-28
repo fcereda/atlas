0ea1e074eaddf372ef2f665b1d6ed768f5b40dd3
Fabio Cereda <fcereda@gmail.com>
2017-12-28 21:12:13 -0200

    Adicionou CPF e NOME_URNA_CANDIDATO à tabela de candidatos

--- scripts/candidatos/candidatos.py
+++ scripts/candidatos/candidatos.py
@@ -1,34 +1,44 @@
 import sys
+import time
 from urllib.request import urlopen, urlretrieve 
 import json
 import gzip
 import csv
+
 
 def urlGzipToJson (url):
     response = urlopen(url).read()
     #print (response)
     #exit()
     try:
-    	tmp = gzip.decompress(response).decode('utf-8')
+        tmp = gzip.decompress(response).decode('utf-8')
     except: 
-    	tmp = response.decode('utf-8')	
+        tmp = response.decode('utf-8')  
     repo = json.loads(tmp)
     return repo
 
 def urlToJson (url):
-	response = urlopen(url).read()
-	tmp = response.decode('utf-8')
-	repo = json.loads(tmp)
-	return repo
+    response = urlopen(url).read()
+    tmp = response.decode('utf-8')
+    repo = json.loads(tmp)
+    return repo
 
 def loadUfs ():
-	url = 'http://servicodados.ibge.gov.br/api/v1/localidades/estados'
-	data = urlGzipToJson(url)
-	return data
+    url = 'http://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    data = urlGzipToJson(url)
+    return data
 
 def loadCSV (url):
-    #response = urlopen(url)
-    (csvFileName, headers) = urlretrieve(url)
+    time.sleep(10)
+
+    # The following code works when CEPESP's API is in the mood:
+    try:
+        (csvFileName, headers) = urlretrieve(url)
+    except Error:
+        print('Error trying to retrieve ' + url)
+        print(Error)
+        exit()  
+
     csvFile = open(csvFileName, 'rt')
     rows = csv.DictReader(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
@@ -38,14 +48,35 @@ def loadCSV (url):
         candidato = {}
         for key in row:
             candidato[key] = row[key]
+        #print(candidato)    
         candidatos.append(candidato)    
 
+    csvFile.close()
     return candidatos     
 
 
 def getUrl (ano, cargo):
+    #url = 'http://cepesp.io/api/consulta/tse?ano={}&cargo={}'.format(ano, cargo)
     url = 'http://cepesp.io/api/consulta/tse?ano={}&cargo={}&agregacao_regional=1&agregacao_politica=2'.format(ano, cargo)
+    campos = [
+        'ANO_ELEICAO',
+        'NUM_TURNO',
+        'UF',  
+        'CODIGO_CARGO',
+        'NUMERO_CANDIDATO',
+        'NOME_CANDIDATO',
+        'CPF_CANDIDATO',
+        'NOME_URNA_CANDIDATO',
+        'NUMERO_PARTIDO',
+        'SIGLA_PARTIDO',
+        'COD_SIT_TOT_TURNO',
+        'DESC_SIT_TOT_TURNO',
+        'QTDE_VOTOS' ]
+
+    for campo in campos:
+        url += '&selected_columns[]=' + campo
     return url
+
 
 def saveDictToFile (dict, fileName):
     file = open(fileName, 'w')
@@ -60,7 +91,7 @@ def saveAsCSV (list, fileName, headers=[]):
         print(headers)
 
     with open(fileName, 'w', encoding='utf-8') as csvfile:
-        fileWriter = csv.writer(csvfile, delimiter=',', quotechar='¨', quoting=csv.QUOTE_MINIMAL)
+        fileWriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         fileWriter.writerow(headers)
         for item in list:
             #print('salvando:')
@@ -91,6 +122,14 @@ def groupByUf (candidatos):
     return ufs    
 
 
+def adjustCandidates (candidatos):
+    for candidato in candidatos:
+        candidato['CPF_CANDIDATO'] = candidato['CPF_CANDIDATO'].zfill(11)
+        if (candidato['NOME_URNA_CANDIDATO'] == '#NULO#'):
+            candidato['NOME_URNA_CANDIDATO'] = candidato['SIGLA_PARTIDO'] + ' (legenda)'
+    return candidatos       
+
+
 def addRanking (candidatos):
 
     def getKey(candidato):
@@ -114,9 +153,10 @@ def addRanking (candidatos):
 
 
 def runATest ():
-    candidatos = loadCSV(getUrl(2016, 11))
+    candidatos = loadCSV(getUrl(2014, 6))
     #for candidato in candidatos:
     #    print(candidato)
+    correctPartyNames(candidatos)
     sortedCandidatos = addRanking(candidatos)
     candidatosSP = groupByUf(sortedCandidatos)['SP1']
     totalVotos = 0
@@ -127,7 +167,7 @@ def runATest ():
     contador = 0
     for candidato in candidatosSP:
         if (contador < 100):
-            print(candidato['CLASSIFICACAO'], candidato['NOME_CANDIDATO'], candidato['UF'],'{:,}'.format(int(float(candidato['QTDE_VOTOS']))),'{0:.2f}%'.format(float(candidato['QTDE_VOTOS'])/totalVotos*100))
+            print(candidato['CLASSIFICACAO'], candidato['NOME_CANDIDATO'], '"' + candidato['NOME_URNA_CANDIDATO'] + '"', candidato['DESC_SIT_TOT_TURNO'], candidato['UF'],'{:,}'.format(int(float(candidato['QTDE_VOTOS']))),'{0:.2f}%'.format(float(candidato['QTDE_VOTOS'])/totalVotos*100))
         contador += 1    
 
     print('Total de votos: {:,}'.format(totalVotos))
@@ -143,7 +183,7 @@ for ano in [1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016]:
     for cargo in [1, 3, 5, 6, 7, 8, 11]:
         sys.stdout.write('Obtendo lista de candidatos para o ano {}, cargo {}...    \r'.format(ano, cargo))
         sys.stdout.flush()
-        candidatos = candidatos + addRanking(loadCSV(getUrl(ano, cargo)))
+        candidatos = candidatos + addRanking(adjustCandidates(loadCSV(getUrl(ano, cargo))))
 print('\nGravando lista de candidatos no arquivo ' + fileName)
 saveAsCSV(candidatos, fileName)
 print('{} linhas foram gravadas no arquivo {}'.format(len(candidatos), fileName))
