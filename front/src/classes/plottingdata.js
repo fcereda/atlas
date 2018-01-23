bbc52cfc893b88dbce9c414299bae59a2dee61d6
Fabio Cereda <fcereda@gmail.com>
2018-01-22 23:08:24 -0200

    Added PlottingData class, to be used by the upcoming MapCharts module

---
+++ front/src/classes/plottingdata.js
@@ -0,0 +1,102 @@
+'use strict'
+
+class PlottingData {
+
+    constructor (colors, data, legend) {
+
+        function plottingDataError (msg) {
+            const ERROR_PREFIX = 'Error in PlottingData constructor: '
+            throw Error(ERROR_PREFIX + msg)
+        }
+        
+        if (!colors || !data) {
+            plottingDataError('missing required arguments')
+        }    
+        if (typeof data != 'object') {
+            plottingDataError('data argument must be a dictionary')
+        }    
+
+        if (!colors.length) {
+            colors = [colors]
+        }    
+        
+        var numColors = colors.length
+        this.colors = colors
+        this.data = Object.keys(data).map(districtId => {
+            let item = data[districtId],    
+                values = item.values ? item.values : item,
+                numValues = values.length
+
+            if (!numValues) {
+                values = [values]
+            }
+            else {
+                if (numValues > numColors) {
+                    plottingDataError('too many values in district ' + districtId)
+                }
+            }
+
+            let sumValues = values.reduce((total, value) => total + value, 0),
+                size = item.size ? item.size : sumValues,
+                points = values.map((value, index) => {
+                    let proportion = value / sumValues
+                    return {
+                        color: colors[index],
+                        value,
+                        proportion,
+                        percentage: Math.round(proportion * 100)
+                    }
+                }),
+                orderedPoints = points.slice().sort((a, b) => b.value - a.value),    
+                //points.slice().sort sorts a copy of the original array, keeping it intact
+                firstTwoPoints = orderedPoints
+            if (orderedPoints.length >= 2) {
+                firstTwoPoints = orderedPoints.slice(0, 2).map(point => {
+                    let proportion = point.value / (orderedPoints[0].value + orderedPoints[1].value)  
+                    return {
+                        ...point,
+                        value,
+                        proportion,
+                        percentage: Math.round(proportion * 100)
+                    }
+                })
+            } 
+ 
+            return {
+                id: districtId,
+                lat: 0,
+                long: 0,
+                size,
+                colors,
+                values,
+                points,
+                orderedPoints,
+                firstTwoPoints
+            }
+        })
+        this.legend = legend
+    }
+
+    get length () {
+        return this.data.length
+    }
+    
+    forEach (callback) {
+        return this.data.forEach(callback)
+    }
+    
+    map (callback) {
+        return this.data.map(callback)
+    }
+    
+    filter (callback) {
+        return this.data.filter(callback)
+    }
+    
+    reduce (callback, initialValue) {
+        return this.data.reduce(callback, initialValue)
+    }
+    
+}
+
+module.exports = PlottingData
